e64c357d23c4fb577268456a2bbbfcd67a8a9b13
Atul Bhatt <atulbhatt61@gmail.com>
2024-02-20 15:29:27 +0530

    read write layers

---
+++ README.md
@@ -0,0 +1,2 @@
+# Interactive Repl
+Read -> Evaluate -> Print
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use eval::eval;
+use std::io;
+
+fn read() -> String {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => input,
+        Err(e) => {
+            println!("Cannot read string: {e}");
+            "".to_string()
+        },
+    }
+}
+
+fn write(input: String) {
+    println!("{input}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!(r#"
+        Welcome to Archer.
+        Use ctrl-c to exit."#);
+
+    loop {
+        print!("> ");
+        write(read());
+        println!();
+    }
 }
